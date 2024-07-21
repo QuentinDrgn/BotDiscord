bbfdb2c637c0a3e338e23ca1b6a17dd33abc8b24
doquentin <quentin.dorigny@ynov.com>
2024-07-21 13:24:29 +0200

    Commit to go public repo

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import "github.com/quentindrgn/BotDiscord"
+import "github.com/QuentinDrgn/BotDiscord"
 
 func main() {
-	bot.BotToken = "MTI2NDUyMzYwMzA1NjI2NzMwNA.Gp1L4u.5eJwycNcm0HwTc_LepJEjhJInXNifxcy4Owae8"
+	bot.BotToken := "secret"
 	bot.Run()
 }
